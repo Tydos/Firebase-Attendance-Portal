f7c5118dddb790d79e3015eb1bab5eeb37d29f13
Prasad Jawale <80669588+Tydos@users.noreply.github.com>
2023-04-17 20:35:35 +0530

    Added Project

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,24 @@
+// Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "",
+//     databaseURL: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: "",
+//     appId: "",
+//     measurementId: ""
+//   };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIO4AU0WfqZ7usCqhKiM-5yJzdY8jyg58",
+  authDomain: "attendance-tracker-6342e.firebaseapp.com",
+  projectId: "attendance-tracker-6342e",
+  storageBucket: "attendance-tracker-6342e.appspot.com",
+  messagingSenderId: "529448287135",
+  appId: "1:529448287135:web:6abd8a033df15cc140dbaa",
+  measurementId: "G-0H0DDMQEBC"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
